08012692f1e58d5c00c53d7a959e30c889dd49fa
Sebastian <sebassperez11@gmail.com>
2024-01-25 09:04:02 -0300

    Actualizacion de animaciones

--- js/index.js
+++ js/index.js
@@ -1,21 +1,17 @@
-const sections = document.querySelectorAll('.section')
-
-
 const showSections = elements => {
     elements.forEach((element) => {
         if (element.isIntersecting) {
-            element.target.classList.remove('hidden')
             element.target.classList.add('show')
         }
     })
 }
 
 const observer = new IntersectionObserver(showSections, {
-    threshold: 0.60
+    threshold: 0.55
 }
 )
 
-const containers = document.querySelectorAll(".container")
+const sections = document.querySelectorAll(".section")
 
 sections.forEach(section => {
     observer.observe(section);
